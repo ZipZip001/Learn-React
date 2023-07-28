2f8afdfb4b6f83ca0415c625f6c98bff63d6c3b9
ZipS <caominh1912@gmail.com>
2023-07-28 11:45:28 +0700

    first typeScript commit

--- Lesson3_Array/lesson3.ts
+++ Lesson3_Array/lesson3.ts
@@ -2,4 +2,4 @@ let name3: string [] = ['Zip', 'Learn- TypeScript']
 
 name3.push("22"); // Phải dùng kiểu dữ liệu Number
 
-console.log(">>> check name 3: ", name3);
+console.log(">>> check name 3: ",   name3);
