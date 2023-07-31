033c63247b58983ae08e802d00f53c88fccc3f18
ZipS <caominh1912@gmail.com>
2023-07-31 21:36:52 +0700

    enum

---
+++ lesson5_Normal/Tuple_types.ts
@@ -0,0 +1,11 @@
+// tuple: dùng để phân định rõ đúng kiểu giá trị và trình tự của nó
+// dataTypes, order, size
+
+let skills: [string, number] = ["Cao Minh", 2,];
+
+let skills2: [string, number, boolean] = ["Cao Minh", 2, true];
+
+// Optional Tuple Elements 
+// Dữ liệu tại đó có thể rỗng và chỉ có thằng cuối sử dụng được
+
+let skills3: [string, number, boolean] = ["Cao Minh", 2, true];
