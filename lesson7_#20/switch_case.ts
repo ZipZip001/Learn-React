b42e93e0f66df37c71bf110a5631136f890ddc1d
ZipS <caominh1912@gmail.com>
2023-08-11 18:45:36 +0700

    finsh lesson 7_#20

---
+++ lesson7_#20/switch_case.ts
@@ -0,0 +1,16 @@
+const ageSC : number = 22
+
+switch ( ageSC ) {
+    case 19:
+    case 18:
+        console.log(" Graduate hight school ");
+        break;
+    case 22:
+        console.log(" Graduate university ");
+        break;
+    case 23:
+        console.log(" Find a real job ");
+        break;
+    default:
+        console.log(" All option is wrong ");
+    }
